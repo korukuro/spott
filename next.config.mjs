60be411b19714ff2ff65dd66e1f5247e0b08ed5e
Siddharth Singh Tomar <siddharth.tomar.ug22@nsut.ac.in>
2026-06-10 16:36:04 +0530

    fix bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   images: {
-    domains: ["images.unsplash.com"]
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        port: "",
+        pathname: "/**", // Allows all image paths from Clerk
+      },
+      // 💡 If you use other image hosts (like Unsplash or AWS S3), 
+      // just copy the block above and change the hostname!
+    ],
+  },
 };
 
 export default nextConfig;
